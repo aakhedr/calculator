e876255f39b2ead72bd32fedf9c9fc6fb970518d
Ahmed Khedr <ahmed.a.khedr@outlook.com>
2015-04-07 23:48:45 +0200

    Done with second demo (lecture2)

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -20,6 +20,54 @@ class ViewController: UIViewController {
             display.text = display.text! + digit
         } else {
             display.text = digit
+            userInTheMiddleOfTypingANumber = true
+        }
+    }
+
+    @IBAction func oeprate(sender: UIButton) {
+        let operation = sender.currentTitle!
+        if userInTheMiddleOfTypingANumber {
+            enter()
+        }
+        switch operation {
+            case "×": performOperation { $0 * $1 }
+            case "÷": performOperation { $1 / $0 }
+            case "+": performOperation { $0 + $1 }
+            case "-": performOperation { $1 - $0 }
+            case "√": performOperation { sqrt($0) }
+            default: break
+        }
+    }
+
+    func performOperation(operation: (Double, Double) -> Double) {
+        if operandStack.count >= 2 {
+            displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
+            enter()
+        }
+    }
+    
+    func performOperation(operation: Double -> Double) {
+        if operandStack.count >= 1 {
+            displayValue = operation(operandStack.removeLast())
+            enter()
+        }
+    }
+    
+    var operandStack = Array<Double>()
+    
+    @IBAction func enter() {
+        userInTheMiddleOfTypingANumber = false
+        operandStack.append(displayValue)
+        println("operandStack = \(operandStack)")
+    }
+    
+    var displayValue: Double {
+        get {
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set {
+            display.text = "\(newValue)"
+            userInTheMiddleOfTypingANumber = false
         }
     }
 }
